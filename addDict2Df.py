f46ae05e67bd31ea3350b5ab7f650a56a8d40c70
Chris Bee <christopheraliashotti@gmail.com>
2022-04-03 01:04:49 +0200

    Add some features

--- addDict2Df.py
+++ addDict2Df.py
@@ -1,23 +1,38 @@
 from pandas import DataFrame
+from matplotlib.pyplot import get
+from youtube_transcript_api import YouTubeTranscriptApi
 
 data = {
     'title': [],
+    'Youtube ID': [],
     'views': [],
     'description': [],
     'date_published': [],
     'duration': [],
     'tags': [],
+    'transcript': [],
+    'transcript language': [],
+    'transcript auto-generated': [],
+    'transcript translatable': [],
 }
 
 df = DataFrame(data)
 
-def addDict2Df(metadata_dict):
+def addDict2Df(metadata_dict, id):
+    transcript_list = YouTubeTranscriptApi.list_transcripts(id)
+    transcript = transcript_list.find_transcript(['de', 'en'])
+
     title = metadata_dict.get("title")
     views = metadata_dict.get("views")
     description = metadata_dict.get("description")
     date_published = metadata_dict.get("date_published")
     duration = metadata_dict.get("duration")
     tags = metadata_dict.get("tags")
-    df.loc[df.shape[0]] = [title, views, description, date_published, duration, tags]
+    transcript_fetch = transcript.fetch()
+    transcript_language = transcript.language_code
+    transcript_auto_generated = transcript.is_generated
+    transcript_translatable = transcript.is_translatable
+
+    df.loc[df.shape[0]] = [title, id,  views, description, date_published, duration, tags, transcript_fetch, transcript_language, transcript_auto_generated, transcript_translatable]
     print(df)
     return df
--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from pytube import Channel
 import pandas as pd
 
 
+
 #beispiel
-channel_url= 'https://www.youtube.com/channel/UCGQFj8C3sMhxuFJjkANkyKA/videos'
+channel_url= "https://www.youtube.com/channel/UCysE7uKxMA9g7u_6Zzf2iKQ/videos"
 
 df = pd.DataFrame()
 channel = Channel(channel_url)
@@ -15,10 +16,11 @@ for url in channel.video_urls:
     counter = counter +1
     print('Download metadata ' + str(counter) + ' of ' + str(len(channel.video_urls)))
     print(url)
+    id = url.replace("https://www.youtube.com/watch?v=", "")
+    print(id)
     dict_video = get_video_info(url)
-    df = addDict2Df(dict_video)
-print(df)
-filename = "SchöpferwissenTV"+".csv"
+    df = addDict2Df(dict_video, id)
+filename = "Der Weg in deine Freiheit"+".csv"
 df.to_csv(filename, index=False)
 
 
