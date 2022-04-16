ef182862933f48c4731f32e526f7ca7bf1400759
arvog <arne.vogelgesang@gmx.net>
2022-04-16 17:21:00 +0200

    adapted manual csv format

--- main.py
+++ main.py
@@ -22,28 +22,23 @@ class CollectChannelData:
 
 
 
-    def create_empty_csv(self, featrues):
+    def create_empty_csv(self, features):
         #TODO: Excetion handeln.... csv File überschreiben.....
  
         channel = Channel(self.channel_url)
         channel_name = channel.channel_name
-        filename= "DATA/"+ channel_name + "_manuel.csv"
+        filename= "DATA/"+ channel_name + "_manuell.csv"
         try:
             df = pd.DataFrame(filename)
             print("File already exists. Use self.update for adding")
         except:
-            df = pd.DataFrame(columns=featrues)
-            listofzeros = [0] * len(featrues)
+            df = pd.DataFrame(columns=features)
+            listofblanks = [' '] * len(features)
             for url in channel.video_urls:
-                id = url.replace("https://www.youtube.com/watch?v=", "")
-                print(id)
-                yt = YouTube(url)
-                title = yt.title
-                date = yt.publish_date
-                new_row = listofzeros
-                new_row[0] = date
-                new_row[1] = id
-                new_row[2] = title
+                video_id = url.replace("https://www.youtube.com/watch?v=", "")
+                print(video_id)
+                new_row = listofblanks
+                new_row[0] = video_id
                 df.loc[len(df.index)]= new_row
             df.to_csv(filename, index=False)
             print("NEW Filename: " + filename)
@@ -94,12 +89,12 @@ class CollectChannelData:
             
 if __name__ == '__main__':
 
-    features =  ["publish_date","id", "title", "sentiments", "effects", "sound", "behaviour", "tags", "people", "location"]
+    features =  ["id", "sentiments", "effects", "sound", "behaviour", "tags", "people", "location", "CDS"]
     #HIER KANAL AUSWÄHLEN
     #Hauptkanal "Schöpferwissen TV": 938 Videos
     Schoepf_channel_url="https://www.youtube.com/channel/UCGQFj8C3sMhxuFJjkANkyKA/videos"
     #Nebenkanal "Der Wahrheit verpflichtet": 994 Videos
-    Warheit_channel_url="https://www.youtube.com/c/DerWahrheitverpflichtet"
+    Wahrheit_channel_url="https://www.youtube.com/c/DerWahrheitverpflichtet"
     #Nebenkanal "Rettung der Menschheit TV": 92 Videos
     Rettung_channel_url="https://www.youtube.com/channel/UC5ZGCLwrKIJrdhYwHYcXCvA/videos"
     #Nebenkanal "Der Weg in deine Freiheit": 32 Videos
@@ -112,41 +107,25 @@ if __name__ == '__main__':
     Drachentoeter_channel_url="https://www.youtube.com/channel/UC4StsLMnfcOuQ374_oGa5sg/videos"
     #Nebenkanal "UBC TV": 11 Videos
     UBC_channel_url="https://www.youtube.com/channel/UCWELnCGV_IYWctpTPiB8-Sw"
+    all_channels = [
+        Schoepf_channel_url,
+        Wahrheit_channel_url,
+        Rettung_channel_url,
+        Freiheit_channel_url,
+        ALLES_channel_url,
+        VEITCLUB_channel_url,
+        Drachentoeter_channel_url,
+        UBC_channel_url
+    ]
     #EINE CSV ERSTELLEN
-    collect_channel_data = CollectChannelData(VEITCLUB_channel_url)
-    collect_channel_data.update()
-
+    #collect_channel_data = CollectChannelData(VEITCLUB_channel_url)
+    #collect_channel_data.update()
 
 
     #Multiple CSVs erstellen
-    """"
-    all_channels= [
-    Schoepf_channel_url, 
-    Warheit_channel_url, 
-    Rettung_channel_url, 
-    Freiheit_channel_url, 
-    ALLES_channel_url,
-    VEITCLUB_channel_url,
-    Drachentoeter_channel_url,
-    UBC_channel_url
-    ]
-    """
-    all_channels= [
-    Rettung_channel_url, 
-    Freiheit_channel_url, 
-    ALLES_channel_url,
-    VEITCLUB_channel_url,
-    Drachentoeter_channel_url,
-    UBC_channel_url
-    ]
 
-
-    """
     for item in all_channels:
         collect_channel_data = CollectChannelData(item)
-        collect_channel_data.process_channel()
+        #collect_channel_data.process_channel()
         collect_channel_data.create_empty_csv(features)
     print("FERTIG HAT ALLES GEKLAPPT")
-    
-    #collect_channel_data.create_empty_csv(features)
-    """
