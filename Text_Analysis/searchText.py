8ae5074b1dce7d945f03d0d69ceaa82977e49b3c
arvog <31415602+arvog@users.noreply.github.com>
2022-04-19 13:44:48 +0200

    replaced os with Path, adjusted output of results

--- Text_Analysis/searchText.py
+++ Text_Analysis/searchText.py
@@ -1,4 +1,5 @@
-import os, glob, re
+import re
+from pathlib import PurePath, Path
 from textblob_de import TextBlobDE
 
 """
@@ -8,11 +9,10 @@ new search
 class searchText:
     def __init__(self, keyword, folder2parse, extension="*.transcript"):
         self.extension = extension
-        self.folder2parse = folder2parse
+        self.folder2parse = Path(folder2parse)
         self.list_all_texts = []
         self.all_text_files()
         self.keyword = keyword
-        print(type(self.list_all_texts))
         self.numberOfMentions = 0
         self.keywordPositions = 0
         self.list_keywords = []
@@ -25,28 +25,25 @@ class searchText:
 
     def getSentences(self):
         for key in self.list_keywords:
-            with open(key[0], "r") as f:
+            with open(*key, "r") as f:
                 data = f.read()
             blob = TextBlobDE(data)
             counter = 0
             for sentence in blob.sentences:
                 if self.keyword in sentence:
-                    print("_"*80)
-                    print("<3"*40)
-                    id = key[0][-23:-11]
-                    print("Find keyword in: " + id)
-                    print("sentence with keyword: " )
-                    self.list_sentences.append({"index":counter, "id":str(id), "result":str(sentence)})
+                    #print("_"*80)
+                    #print("<3"*40)
+                    id = str(*key)[-23:-11]
+                    #print("sentence with keyword: " )
+                    self.list_sentences.append({"index":counter, "id":id, "result":str(sentence)})
                     counter += 1
-                    print("_"*80)
-                    print("<3"*40)
+                    #print("_"*80)
+                    #print("<3"*40)
 
     def all_text_files(self):
         all_text_files = []
-        for path, subdir, files in os.walk(self.folder2parse):
-            search_path = os.path.join(path, self.extension)
-            for file in glob.glob(search_path):
-                all_text_files.append(file)
+        for file in self.folder2parse.glob(self.extension):
+            all_text_files.append(file)
         self.list_all_texts = all_text_files
 
     def search4keyword(self):
@@ -56,17 +53,17 @@ class searchText:
         for text in self.list_all_texts:
             with open(text, "r") as f:
                 data = f.read()
-            startKeyword =data.find(self.keyword, 0)
+            startKeyword = data.find(self.keyword, 0)
             if startKeyword != -1:
                 numberOfMentions += 1
             positions = [m.start() for m in re.finditer(self.keyword, data)]
             if positions != []:
-                list_keywords.append([text, positions])
+                list_keywords.append({text: positions})
         self.numberOfMentions = numberOfMentions
         self.list_keywords = list_keywords
 
 
 if __name__ == "__main__":
-    folder2parse = "."
+    folder2parse = r"\\DATEN\Schöpferwissen\UBC TV"
     #extension is set to "-transcript" by default
-    search = searchText("Corona", folder2parse)
+    search = searchText("hallo", folder2parse)
